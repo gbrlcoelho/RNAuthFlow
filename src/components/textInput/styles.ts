21b1cc1eaefce1791fda8e4dd6a927b41b64f47d
gccano <gccano@viptech.com.br>
2022-07-14 17:09:16 -0300

    feat(components): create a generic text input component

---
+++ src/components/textInput/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components/native'
+
+export const CustomTextInput = styled.TextInput`
+  border-radius: 8;
+  border-color: #1d013f;
+  padding: 0px 8px 0px 8px;
+  color: #000;
+  width: 100%;
+  height: 50px;
+  margin-bottom: 16px;
+`
