761cd66c89a2804ebfd1a6f38c5e11a0ae41c0ef
Gabriel Coelho <coelhogabriel67@gmail.com>
2022-07-16 15:10:53 -0300

    refactor(screens): change styles on Home screen

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components/native'
+import {BoldTextProps, HomeViewProps} from './types'
+
+export const HomeView = styled.View<HomeViewProps>`
+  flex: ${(props) => (props.flex ? props.flex : 'none')};
+  justify-content: center;
+  align-items: center;
+  top: ${(props) => (props.top ? props.top : 0)};
+  padding: 0px 16px 0px 16px;
+`
+export const HomeText = styled.Text<BoldTextProps>`
+  font-weight: ${(props) => (props.bold ? 'bold' : 'none')};
+  font-size: ${(props) => (props.fontSize ? props.fontSize : 10)};
+  color: ${(props) => (props.color ? props.color : '#000')};
+`
