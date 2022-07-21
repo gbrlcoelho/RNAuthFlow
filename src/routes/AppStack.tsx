1e47a0168c7ea693db6469f0ed487b5e26467bfb
gccano <gccano@viptech.com.br>
2022-07-21 16:14:34 -0300

    refactor(routes): useTheme hook on stack screen

--- src/routes/AppStack.tsx
+++ src/routes/AppStack.tsx
@@ -1,15 +1,17 @@
 import {createNativeStackNavigator} from '@react-navigation/native-stack'
 import React from 'react'
+import {useTheme} from 'styled-components/native'
 import {Home} from '../screens/Home'
 import {SettingsScreen} from '../screens/Settings'
 
 const Stack = createNativeStackNavigator()
 
 export const AppStack = () => {
+  const {colors} = useTheme()
   return (
     <Stack.Navigator>
-      <Stack.Screen options={{headerTitleAlign: 'center'}} name='Home' component={Home} />
-      <Stack.Screen options={{headerTitleAlign: 'center'}} name='Settings' component={SettingsScreen} />
+      <Stack.Screen options={{headerTintColor: colors.onBackground, headerTitleAlign: 'center', headerStyle: {backgroundColor: colors.background}}} name='Home' component={Home} />
+      <Stack.Screen options={{headerTintColor: colors.onBackground, headerTitleAlign: 'center', headerStyle: {backgroundColor: colors.background}}} name='Settings' component={SettingsScreen} />
     </Stack.Navigator>
   )
 }
--- src/routes/AuthStack.tsx
+++ src/routes/AuthStack.tsx
@@ -1,13 +1,15 @@
 import {createNativeStackNavigator} from '@react-navigation/native-stack'
 import React from 'react'
+import {useTheme} from 'styled-components/native'
 import {SignIn} from '../screens/SignIn'
 
 const Stack = createNativeStackNavigator()
 
 export const AuthStack = () => {
+  const {colors} = useTheme()
   return (
     <Stack.Navigator>
-      <Stack.Screen options={{headerTitleAlign: 'center'}} name='Sign In' component={SignIn} />
+      <Stack.Screen options={{headerTintColor: colors.onBackground, headerTitleAlign: 'center', headerStyle: {backgroundColor: colors.background}}} name='Sign In' component={SignIn} />
     </Stack.Navigator>
   )
 }
