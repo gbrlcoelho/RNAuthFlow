1e1c409f3f803563b6cb59d51fcf030f4dd2d107
gccano <gccano@viptech.com.br>
2022-07-21 16:15:39 -0300

    feat(theme): create light and dark theme

---
+++ src/theme/darkTheme.ts
@@ -0,0 +1,9 @@
+import {DefaultTheme} from 'styled-components/native'
+
+export const darkTheme: DefaultTheme = {
+  colors: {
+    background: '#121212',
+    onBackground: '#fff',
+    primary: '#00264B',
+  },
+}
