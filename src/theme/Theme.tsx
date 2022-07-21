3e79c038c4a2695fccb1abbbb90f22667fa93ec8
gccano <gccano@viptech.com.br>
2022-07-21 16:56:14 -0300

    feat(theme): add async storage to persist theme when app is closed

--- src/theme/Theme.tsx
+++ src/theme/Theme.tsx
@@ -1,10 +1,11 @@
-import React, {createContext, useState} from 'react'
+import React, {createContext, useEffect, useState} from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import {ChildrenProps} from '../contexts/types'
 import {ThemeProvider as ThemeProviderStyled} from 'styled-components/native'
 import {darkTheme} from './darkTheme'
 import {lightTheme} from './lightTheme'
 
-enum ThemeType {
+export enum ThemeType {
   light = 'light',
   dark = 'dark',
 }
@@ -22,13 +23,27 @@ export const ThemeContext = createContext({
 export const ThemeProvider: React.FC<ChildrenProps> = ({children}) => {
   const [theme, setTheme] = useState(ThemeType.light)
 
-  function toggleTheme() {
-    if (theme === ThemeType.light) {
-      setTheme(ThemeType.dark)
-    } else {
-      setTheme(ThemeType.light)
+  async function loadTheme() {
+    const savedTheme = await AsyncStorage.getItem('@theme')
+    if (savedTheme) {
+      setTheme(savedTheme)
     }
   }
+
+  function toggleTheme() {
+    let selectTheme
+    if (theme === ThemeType.light) {
+      selectTheme = ThemeType.dark
+    } else {
+      selectTheme = ThemeType.light
+    }
+    setTheme(selectTheme)
+    AsyncStorage.setItem('@theme', selectTheme)
+  }
+
+  useEffect(() => {
+    loadTheme()
+  }, [])
   return (
     <ThemeContext.Provider value={{theme, toggleTheme}}>
       <ThemeProviderStyled theme={themes[theme]}>{children}</ThemeProviderStyled>
